24c5be5baba8740ed23a25b7dbef190d905b351d
Christine Susic <csusic022@insite.4cd.edu>
2026-02-13 18:37:55 -0800

    Commit

--- main.cpp
+++ main.cpp
@@ -3,35 +3,26 @@
 #include <iostream>
 using namespace std;
 
-void reverseArray(string []);
 string* displayArray(string []);
+void reverseArray(string []);
 
 //size of array
 const int SIZE = 5;
 
 int main() {
-    //string pointer that points to new array
+    //names array
+    string names[SIZE] = {"Frieda", "Jane", "Alfie", "Leonard", "Penny"};
+    //string pointer that points to names array
     string *ptr = nullptr;
-    //new string array
-    ptr = new string[SIZE];
-    string arr[SIZE] = {"Frieda", "Jane", "Alfie", "Leonard", "Penny"};
+    ptr = names;
     
     
-    reverseArray(ptr);
     displayArray(ptr);
+    reverseArray(ptr);
     
-    delete [] ptr; //delete array 
+    //delete [] ptr; //delete array 
    
     return 0;
-}
-
-//recieves dynamic string array
-//reverses the elements
-//returns a pointer to modified array
-void reverseArray(string arr[SIZE]) {
-    for (int i = 0; i < SIZE; i++) {
-        cout << arr[i] << " ";
-    }
 }
 
 //receive dynamic string array
@@ -41,4 +32,13 @@ string* displayArray(string arr[SIZE]) {
         cout << arr[i] << " ";
     } 
     return arr;
+}
+
+//recieves dynamic string array
+//reverses the elements
+//returns a pointer to modified array
+void reverseArray(string arr[SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        cout << arr[i] << " ";
+    }
 }
